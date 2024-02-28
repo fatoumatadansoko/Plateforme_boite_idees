76ed104b20b3de2907a776271a4d300596d23f6b
fatoumata dansoko <fatoumatadansoko61@gmail.com>
2024-02-28 18:52:40 +0000

    ici on retrouve le  fichier accueil avec le lle style.css avec le dossier img  qui qui m'ont permise d'avoir la page d'acceuil

---
+++ acceuil.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>InnovateTech</title>
+</head>
+<body>
+    <header>
+    <h1>InnovateTech</h1>
+                <div class="menu">
+                     <a href="index.php">Accueil</a>
+                     <a href="inscription.php">INSCRIPTION</a>
+                     <a href="boite_idees.php">Vos_idées</a>
+               </div>
+    </header>
+
+
+         <div class="head">
+    <img src="img/picture.jpg" alt="">
+    <a href="inscription.php"> Sinscrire</a>
+    <a href=""> Connexion</a>
+
+         </div>
+    
+</body>
+</html>
