a661b8f59931e4e3ef53defb3742eea8fd6ed9d2
fatoumata dansoko <fatoumatadansoko61@gmail.com>
2024-02-29 10:02:28 +0000

    ajout de la page inscription avec un formulaire d'inscription

---
+++ inscription.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>INSCRIPTION</title>
+</head>
+<body>
+
+
+</div>
+    <p>
+         <h2>Bienvenue!</h2>
+         <h2>Inscrivez_vous d'abord</h2>
+    
+    </p>
+    <form action="traitement.php" method="POST">
+        Nom*: <input type="text" name="nom" required> <br>
+        Prénom*: <input type="text" name="prenom" required> <br>
+        Email*: <input type="text" name="mail" required> <br>
+        Mot de passe*: <input type="password" name="pass" required> <br>
+        <input type="submit" >
+
+    </form>
+    
+</body>
+</html>
